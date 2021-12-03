8418fb40eb23a54c74641cb54de101329edeed84
Joseph Mignone <jmig3066@gmail.com>
2021-12-02 21:58:41 -0500

    Update config.cs

--- WsaGappsTool_src/WsaGappsTool/config.cs
+++ WsaGappsTool_src/WsaGappsTool/config.cs
@@ -8,14 +8,10 @@ namespace WsaGappsTool
 {
     public static class config
     {
-        public static string AppName = "Wsa Gapps Tool";
-
         public static string sevenZip_Ex = "../tools/7z/7z.exe";
         public static string qemu_Ex = "../qemu/qemu-system-x86_64.exe";
         public static string vm_dir = "../vm/";
         public static string vm_dataDiskImage = vm_dir + "data.vhdx";
-
-        public static string vm_dataDiskImage_MountedVolumeName = "WSA_FILES";
 
         public static string CacheDirectory = "cache/";
     }
